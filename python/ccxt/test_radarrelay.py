60c5486c9e7b95cb4606cf32979a880372cac89d
bweick <brian@biometrixtech.com>
2017-12-25 15:43:22 -0700

    Updated tests for dynamic precision calculations.

--- python/ccxt/test_radarrelay.py
+++ python/ccxt/test_radarrelay.py
@@ -15,6 +15,15 @@ class TestRadarRelayCCXT(TestCase):
         with open(os.getcwd() +'/orderbook_data.json') as order_data:
             self.exchange.order_bk = json.load(order_data)
 
+        self.exchange.markets = {'req/eth':{'symbol':'req/eth',
+            'quoteTokenAddress':"0x2956356cd2a2bf3202f771f50d3d14a367b48070",
+            'baseTokenAddress': "0x8f8221afbb33998d8584a2b05749ba73c37a938a",
+            'precision': {
+                'amountBase': 10**18,
+                'amountQuote': 10**18
+            }
+        }}
+
     def test_set_wallet(self):
         self.fail("create set_wallet function.")
 
@@ -87,7 +96,7 @@ class TestRadarRelayCCXT(TestCase):
     ###TESTS FOR ANALYZE_ORDERBOOK METHOD
     @patch('radarrelay.radarrelay._fill_order')
     def test_analyze_orderbook_fillable(self, mock_fill):
-        pending, leftover = self.exchange._analyze_orderbook('sell', 0.000129, 2000)
+        pending, leftover = self.exchange._analyze_orderbook('req/eth', 'sell', 0.000129, 2000)
 
         self.assertEqual(leftover, 0)
         self.assertEqual(pending, ['0x12459c951127e0c374ff9105dda097662a027093', '0x12459c951127e0c374ff9105dda097662a027093'])
@@ -96,7 +105,7 @@ class TestRadarRelayCCXT(TestCase):
 
     @patch('radarrelay.radarrelay._fill_order')
     def test_analyze_orderbook_unfillable(self, mock_fill):
-        pending, leftover = self.exchange._analyze_orderbook('sell', 0.00015, 2000)
+        pending, leftover = self.exchange._analyze_orderbook('req/eth', 'sell', 0.00015, 2000)
 
         self.assertEqual((leftover)/(10**18), 2000)
         self.assertEqual(pending, [])
@@ -105,7 +114,7 @@ class TestRadarRelayCCXT(TestCase):
 
     @patch('radarrelay.radarrelay._fill_order')
     def test_analyze_orderbook_partial_fill(self, mock_fill):
-        pending, leftover = self.exchange._analyze_orderbook('buy', 0.000176, 15000)
+        pending, leftover = self.exchange._analyze_orderbook('req/eth', 'buy', 0.000176, 15000)
 
         self.assertEqual((leftover)/(10**18), 5000)
         self.assertEqual(pending, ['0x12459c951127e0c374ff9105dda097662a027093'])
@@ -114,7 +123,7 @@ class TestRadarRelayCCXT(TestCase):
 
     @patch('radarrelay.radarrelay._fill_order')
     def test_analyze_orderbook_market(self, mock_fill):
-        pending, leftover = self.exchange._analyze_orderbook('buy', float('inf'), 15000)
+        pending, leftover = self.exchange._analyze_orderbook('req/eth', 'buy', float('inf'), 15000)
 
         self.assertEqual(leftover, 0)
         self.assertEqual(pending, ['0x12459c951127e0c374ff9105dda097662a027093', '0x12459c951127e0c374ff9105dda097662a027093'])
@@ -155,14 +164,20 @@ class TestRadarRelayCCXTOrderMath(TestCase):
           }
         }
 
+        self.markets = {'req/eth':{'symbol':'req/eth',
+            'quoteTokenAddress':"0x2956356cd2a2bf3202f771f50d3d14a367b48070",
+            'baseTokenAddress': "0x8f8221afbb33998d8584a2b05749ba73c37a938a",
+            'precision': {
+                'amountBase': 10**18,
+                'amountQuote': 10**18
+            }
+        }}
+
     ###TESTS FOR SELL_POST AND BUY_POST
     @patch('radarrelay.radarrelay.load_markets', return_value={})
     def test_buy_post(self, mock_load):
         self.exchange.wallet = ['0x9e56625509c2f60af937f23b7b532600390e8c8b']
-        self.exchange.markets = {'req/eth':{'symbol':'req/eth',
-            'quoteTokenAddress':"0x2956356cd2a2bf3202f771f50d3d14a367b48070",
-            'baseTokenAddress': "0x8f8221afbb33998d8584a2b05749ba73c37a938a"
-        }}
+        self.exchange.markets = self.markets
 
         with patch.object(self.exchange, 'public_post_fees') as mock_fees:
             mock_fees.return_value = self.fees
